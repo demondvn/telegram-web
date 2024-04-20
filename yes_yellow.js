169b962ef774d64eacccbc337fcdb3a0a280e017
MonPham <demondhunter93@gmail.com>
2024-04-20 10:13:28 +0700

    Update yes_yellow.js

--- yes_yellow.js
+++ yes_yellow.js
@@ -1,6 +1,6 @@
 const sessionStorageValue = sessionStorage.getItem('telegram-mini-apps-launch-params');
-const decodedValue = decodeURIComponent(sessionStorageValue);
-const startIndex = decodedValue.indexOf('tgWebAppData=') + 13;
-const endIndex = decodedValue.indexOf('&tgWebAppPlatform', startIndex);
-const extractedString = decodedValue.substring(startIndex, endIndex);
+// const decodedValue = decodeURIComponent(sessionStorageValue);
+const startIndex = sessionStorageValue.indexOf('tgWebAppData=') + 13;
+const endIndex = sessionStorageValue.indexOf('&tgWebAppPlatform', startIndex);
+const extractedString = sessionStorageValue.substring(startIndex, endIndex);
 console.log(extractedString);
